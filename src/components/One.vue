99b5a4c465537fdff303874487c1a9eaac3d1940
luckystar12 <2812357150@qq.com>
2017-12-06 17:22:01 +0800

    初始化项目

---
+++ src/components/One.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h1 v-bind:title="msg">hi</h1>
+    <p v-if="seen">现在你看到我了</p>
+    <ol>
+      <li v-for="todo in todos">
+        {{ todo.text }}
+      </li>
+    </ol>
+    <ol>
+      <li v-for="user in userlist">
+        名字：{{user.name}} <br/>
+        创建时间： {{user.createTime}}
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      seen: true,
+      todos: [
+        { text: '学习 JavaScript' },
+        { text: '学习 Vue' },
+        { text: '整个牛项目' }
+      ],
+      userlist: []
+    }
+  },
+  http: {
+    // root: '',
+    headers: {
+      // Authorization: '',
+      'Access-Control-Allow-Origin': 'http://localhost:3000'
+    },
+    emulateJSON: true,
+    emulateHTTP: true
+  },
+  mounted: function () {
+    // GET /someUrl
+    this.$http.get('/user/test/lym').then(response => {
+      console.log(response.data)
+      // get body data
+      this.userlist = response.data
+    }, response => {
+      console.log('error')
+    })
+  }
+}
+</script>
